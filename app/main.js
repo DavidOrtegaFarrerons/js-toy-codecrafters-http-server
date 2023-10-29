79dc5ae7454ada1214ecaa8890da23602c808c22
David Ortega Farrerons <davidortegafarrerons@gmail.com>
2023-10-29 13:25:50 +0100

    feature (status): Add 404 response when path of request is different from /

--- app/main.js
+++ app/main.js
@@ -2,11 +2,26 @@ const net = require("net");
 
 const PORT = 4221;
 const HOSTNAME = "localhost";
-const server = net.createServer((socket) => {
-    console.log("New connection");
 
+const HTTP_VERSION = "HTTP/1.1";
+const STATUS = {
+    "200": "200 OK",
+    "404": "404 Not Found"
+}
+
+function responseByPath(path) {
+    let status = path === "/" ? STATUS["200"] : STATUS["404"];
+
+    return HTTP_VERSION + " " + status + "\r\n\r\n";
+}
+
+const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        let requestInfo = data.toString();
+        let path = requestInfo.split(' ', 2)[1];
+        let response = responseByPath(path);
+
+        socket.write(response);
         socket.end();
     });
     socket.on("close", () => {
@@ -15,7 +30,5 @@ const server = net.createServer((socket) => {
         console.log("Connection ended");
     });
 });
-
-
 
 server.listen(PORT, HOSTNAME);
