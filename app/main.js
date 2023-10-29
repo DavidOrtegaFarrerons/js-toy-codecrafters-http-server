e28516a8c0c37ac130dc982c32088462fd04c196
David Ortega Farrerons <davidortegafarrerons@gmail.com>
2023-10-29 12:56:16 +0100

    feature (data): make server return HTTP 200 status when getting an incoming request

--- app/main.js
+++ app/main.js
@@ -1,10 +1,21 @@
 const net = require("net");
 
+const PORT = 4221;
+const HOSTNAME = "localhost";
 const server = net.createServer((socket) => {
+    console.log("New connection");
+
+    socket.on("data", (data) => {
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        socket.end();
+    });
     socket.on("close", () => {
         socket.end();
         server.close();
+        console.log("Connection ended");
     });
 });
 
- server.listen(4221, "localhost");
+
+
+server.listen(PORT, HOSTNAME);
